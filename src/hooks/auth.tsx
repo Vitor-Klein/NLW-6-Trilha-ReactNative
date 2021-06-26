bec4dd1ca36186f2f572f6c880742b82d1e9e371
Vitor Klein <vitorhklein2@gmail.com>
2021-06-26 13:57:37 -0300

    finish refactors

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,14 +2,12 @@ import React, { createContext, useContext, useState, ReactNode } from 'react'
 
 import * as AuthSession from 'expo-auth-session'
 
-import { 
-  SCOPE,
-  CLIENT_ID,
-  CDN_IMAGE,
-  REDIRECT_URI,
-  RESPONSE_TYPE
- } from '../configs'
-
+  const { SCOPE } = process.env
+  const { CLIENT_ID } = process.env
+  const { CDN_IMAGE } = process.env
+  const { REDIRECT_URI } = process.env
+  const { RESPONSE_TYPE } = process.env
+  
 import { api } from '../services/api'
 
 type User = {
@@ -33,7 +31,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
-    access_token: string
+    access_token?: string
+    error?: string
   }
 }
 
@@ -52,7 +51,7 @@ function AuthProvider({ children }: AuthProviderProps) {
       const { type, params } = await AuthSession
         .startAsync({ authUrl }) as AuthorizationResponse
 
-        if(type === 'success'){
+        if(type === 'success' && !params.error){
           api.defaults.headers.authorization = `Bearer ${params.access_token}`
 
           const userInfo = await api.get('/users/@me')
@@ -65,13 +64,12 @@ function AuthProvider({ children }: AuthProviderProps) {
             firstName,
             token: params.access_token
           })
-          setLoading(false)
-        }else{
-          setLoading(false)
-        }
 
+        }
     } catch {
       throw new Error('nao foi possivel autenticar')
+    } finally {
+      setLoading(false)
     }
   }
 
