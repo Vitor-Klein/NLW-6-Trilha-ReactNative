24f6842d8a62c94651afbab594c4ae73e1e8a20f
Vitor Klein <vitorhklein2@gmail.com>
2021-06-26 00:12:27 -0300

    create context

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
+import { AuthProvider } from './src/hooks/auth'
+
 import { Background } from './src/components/Background';
 import { Routes } from './src/routes';
 
@@ -28,7 +30,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Routes />
+      <AuthProvider> 
+        <Routes />
+      </AuthProvider>
     </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Image, Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 
+import { useAuth } from '../../hooks/auth'
+
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
@@ -10,6 +12,8 @@ import { Background } from '../../components/Background';
 
 export function SignIn() {
     const navigation = useNavigation();
+
+    const { user } = useAuth()
 
     function handleSignIn() {
         navigation.navigate('Home');
